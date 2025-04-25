dc2fd0ac4b53e1c3e6b22739965b582304759f5e
Jesse Clark <hello@developerjesse.com>
2025-04-25 17:08:38 +0300

    Add new release-candidate url.

--- src/controller/httpsAPI.ts
+++ src/controller/httpsAPI.ts
@@ -53,7 +53,8 @@ export class HttpsAPI {
       'https://dev.app.rootstockcollective.xyz',
       'https://qa.cr.rootstockcollective.xyz',
       'https://staging.cr.rootstockcollective.xyz',
-      'https://staging.app.rootstockcollective.xyz'
+      'https://staging.app.rootstockcollective.xyz',
+      'https://release-candidate.app.rootstockcollective.xyz'
     ]
     this.app.use(cors({
       origin: (origin, callback) => {
