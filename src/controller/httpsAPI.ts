4dfb5bc093a9518446b5e7f10c6cb731df62f623
Francis Rodriguez <39339295+Freshenext@users.noreply.github.com>
2025-06-13 10:14:36 -0400

    DAO-1264 Updated cors with new env urls (#38)

--- src/controller/httpsAPI.ts
+++ src/controller/httpsAPI.ts
@@ -56,7 +56,8 @@ export class HttpsAPI {
       'https://qa.dao.rootstockcollective.xyz',
       'https://staging.cr.rootstockcollective.xyz',
       'https://staging.app.rootstockcollective.xyz',
-      'https://release-candidate.app.rootstockcollective.xyz'
+      'https://release-candidate-mainnet.app.rootstockcollective.xyz',
+      'https://release-candidate-testnet.app.rootstockcollective.xyz'
     ]
     this.app.use(cors({
       origin: (origin, callback) => {
